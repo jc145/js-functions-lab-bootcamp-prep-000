25ae2805b00a02d218f53757cd86aca164f886f1
J C <joyce.chiu.415@gmail.com>
2017-10-07 05:11:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-// write your code below!
-function happyHolidays() {
-  return "Happy holidays!"
-}
 
-function happyHolidaysTo('you') {
-  return "Happy holidays, ${name}!"
-}
-
-function happyHolidayTo ('Independence Day', 'you') {
-  return "Happy ${holiday}, ${name}!"
-}
-
-function holidayCountdown ("Mother's Day", '20') {
-  return "It\'s ${days} days until ${holiday}!"
-}
