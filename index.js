8310d0060704f1bc0609517939fe1c6762e6a926
J C <joyce.chiu.415@gmail.com>
2017-10-07 05:37:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // write your code below!
 function happyHolidays(string) {
-  return "Happy holidays!"
+  return 'Happy holidays!'
 }
 say happyHolidays('string')
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, ${name}!"
+  return 'Happy holidays, ${name}!'
 }
 say happyHolidaysTo('you')
 
 function happyHolidayTo(holiday, name) {
-  return "Happy ${holiday}, ${name}!"
+  return 'Happy ${holiday}, ${name}!'
 }
 say happyHolidayTo('Independence Day', 'you')
 
 function holidayCountdown(holiday, days) {
-  return "It\'s ${days} days until ${holiday}!"
+  return 'It\'s ${days} days until ${holiday}!'
 }
 say holidayCountdown("Mother's Day", '20')
