4b5fe61267cfefe6700d30c2ae49a5ad1410f3d0
J C <joyce.chiu.415@gmail.com>
2017-10-07 05:19:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo('you') {
+function happyHolidaysTo(name) {
   return "Happy holidays, ${name}!"
 }
 
-function happyHolidayTo ('Independence Day', 'you') {
+function happyHolidayTo (holiday, name) {
   return "Happy ${holiday}, ${name}!"
 }
 
-function holidayCountdown ("Mother's Day", '20') {
+function holidayCountdown (holiday, days) {
   return "It\'s ${days} days until ${holiday}!"
 }
